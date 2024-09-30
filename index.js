29662762e144bb59466535f8e265ec8bc3a092f7
Mary Monchari Gichana <marygichana95@gmail.com>
2024-09-30 11:00:54 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+const thankYouCards = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(thankYouCards);
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+countDown(9);
